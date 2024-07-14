27682e5c258390e2cf39432b5dbfee726071dbbb
LidaMudota <ktopro4italtotzdohnet@yandex.ru>
2024-07-14 18:16:18 +0300

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = event.target.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
